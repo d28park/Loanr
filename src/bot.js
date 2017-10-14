d5372ff94d5fcd02f1fbc0a04f0612357c3a9a09
Daniel Park <Daniel.Park@nexj.com>
2017-10-14 19:45:27 -0400

    Base UI

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,5 @@
 const Bot = require('./lib/Bot')
+const IDS = require('./lib/IdService')
 const SOFA = require('sofa-js')
 const Fiat = require('./lib/Fiat')
 
@@ -27,19 +28,107 @@ bot.onEvent = function(session, message) {
 }
 
 function onMessage(session, message) {
-  welcome(session)
+  console.log(session.get('stage'))
+  switch (session.get('stage')) {
+    case undefined:
+      sendMessage(session, "Hello, would you like to start a loan contract?")
+      break    
+    case 'initialize':
+      sendMessage(session, "Hello, would you like to start a loan contract?")
+      break
+    case 'set-user':
+      let user = message.content.body
+
+      // TODO - logic to get user and user address
+      if (true) {
+        session.set('user', user)
+        session.set('stage', 'set-eth')
+        session.reply("Please specify the ETH amount:")
+      } else {
+        session.reply("User: " + user + " not found.")
+        session.set('stage', 'initialize')
+      }
+      break
+    case 'set-eth':
+      let eth = message.content.body
+
+      // TODO - check isNumber and <= balance
+      if (true) {
+        session.set('eth', eth)
+        session.set('stage', 'set-interest-rate')
+        session.reply("Please specify the interest rate (%):")
+      } else {
+        session.reply(eth + " is not a valid value.")
+        session.set('stage', 'initialize')
+      }
+      break
+    case 'set-interest-rate':
+      let interest = message.content.body
+
+      // TODO - check isNumber and > 0
+      if (true) {
+        session.set('interest', interest)
+        session.set('stage', 'set-days')
+        session.reply("Please specify the number of remaining days:")
+      } else {
+        session.reply(interest + "% is not a valid value.")
+        session.set('stage', 'initialize')
+      }
+      break
+    case 'set-days':
+      let days = message.content.body
+
+      // TODO - check isNumber and > 0
+      if (true) {
+        session.set('days', days)
+        session.set('stage', 'set-consequence')
+        session.reply("Please specify the consequence:")
+      } else {
+        session.reply(days + " days is not a valid value.")
+        session.set('stage', 'initialize')
+      }
+      break
+    case 'set-consequence':
+      let consequence = message.content.body
+
+      // TODO - check null
+      if (true) {
+        session.set('consequence', consequence)
+        session.set('stage', 'set-loan')
+        sendLoan(session, 
+          "Here are your loan details:\n ETH: " + 
+          session.get('eth') + 
+          "\n Interest Rate: " + 
+          session.get('interest') + 
+          "%\n Due In: " + 
+          session.get('days') + 
+          " days\n\n Send this loan to user " + 
+          session.get('user') + 
+          "?") 
+      } else {
+        session.reply("You must specify a consequence.")
+        session.set('stage', 'initialize')
+      }
+      break
+    case 'set-loan':
+      break
+  }
 }
 
 function onCommand(session, command) {
   switch (command.content.value) {
-    case 'ping':
-      pong(session)
+    case 'Yes':
+      setUser(session)
       break
-    case 'count':
-      count(session)
+    case 'No':
+      session.set('stage', 'initialize')
       break
-    case 'donate':
-      donate(session)
+    case 'CreateLoan':
+      console.log('TRUFFLE')
+      session.set('stage', 'initialize')
+      break
+    case 'CancelLoan':
+      session.set('stage', 'initialize')
       break
     }
 }
@@ -68,39 +157,34 @@ function onPayment(session, message) {
 
 // STATES
 
-function welcome(session) {
-  sendMessage(session, `Hello Token!`)
-}
-
-function pong(session) {
-  sendMessage(session, `Pooong`)
-}
-
-// example of how to store state on each user
-function count(session) {
-  let count = (session.get('count') || 0) + 1
-  session.set('count', count)
-  sendMessage(session, `${count}`)
-}
-
-function donate(session) {
-  // request $1 USD at current exchange rates
-  Fiat.fetch().then((toEth) => {
-    session.requestEth(toEth.USD(1))
-  })
+function setUser(session) {
+  session.set('stage', 'set-user')
+  session.reply('Please specify the user requesting the loan:')
 }
 
 // HELPERS
 
 function sendMessage(session, message) {
-  let controls = [
-    {type: 'button', label: 'Ping', action: 'Webview::https://www.larvalabs.com/cryptopunks/details/5953'},
-    {type: 'button', label: 'Count', value: 'count'},
-    {type: 'button', label: 'Donate', value: 'donate'}
-  ]
   session.reply(SOFA.Message({
     body: message,
-    controls: controls,
+    controls: 
+      [
+        {type: 'button', label: 'Yes', value: 'Yes'},
+        {type: 'button', label: 'No', value: 'No'}
+      ],
     showKeyboard: false,
+  }))
+}
+
+function sendLoan(session, message) {
+  console.log('asd')
+  session.reply(SOFA.Message({
+    body: message,
+    controls:
+      [
+        {type: 'button', label: 'Yes', value: 'CreateLoan'},
+        {type: 'button', label: 'No', value: 'CancelLoan'}
+      ],
+      showKeyboard: false,
   }))
 }
