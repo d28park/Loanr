dbdbecba527ad268a7bd02130471b57cc13964dc
noverlap <kisunji92@gmail.com>
2017-10-14 23:32:56 -0400

    Refactored code

--- src/bot.js
+++ src/bot.js
@@ -5,186 +5,198 @@ const Fiat = require('./lib/Fiat')
 
 let bot = new Bot()
 
-// ROUTING
-
 bot.onEvent = function(session, message) {
-  switch (message.type) {
-    case 'Init':
-      welcome(session)
-      break
-    case 'Message':
-      onMessage(session, message)
-      break
-    case 'Command':
-      onCommand(session, message)
-      break
-    case 'Payment':
-      onPayment(session, message)
-      break
-    case 'PaymentRequest':
-      welcome(session)
-      break
-  }
+	switch (message.type) {
+		case 'Init':
+			welcome(session)
+			break
+		case 'Message':
+			onMessage(session, message)
+			break
+		case 'Command':
+			onCommand(session, message)
+			break
+		case 'Payment':
+			rejectPayment(session, message)
+			break
+		case 'PaymentRequest':
+			rejectRequest(session, message)
+			break
+	}
 }
 
+function welcome(session) {
+	session.reply("Hi! My name is Loanr. Type 'hi' to get started.")
+	return
+}
 function onMessage(session, message) {
-  console.log(session.get('stage'))
-  switch (session.get('stage')) {
-    case undefined:
-      sendMessage(session, "Hello, would you like to start a loan contract?")
-      break    
-    case 'initialize':
-      sendMessage(session, "Hello, would you like to start a loan contract?")
-      break
-    case 'set-user':
-      let user = message.content.body
-
-      // TODO - logic to get user and user address
-      if (true) {
-        session.set('user', user)
-        session.set('stage', 'set-eth')
-        session.reply("Please specify the ETH amount:")
-      } else {
-        session.reply("User: " + user + " not found.")
-        session.set('stage', 'initialize')
-      }
-      break
-    case 'set-eth':
-      let eth = message.content.body
-
-      // TODO - check isNumber and <= balance
-      if (true) {
-        session.set('eth', eth)
-        session.set('stage', 'set-interest-rate')
-        session.reply("Please specify the interest rate (%):")
-      } else {
-        session.reply(eth + " is not a valid value.")
-        session.set('stage', 'initialize')
-      }
-      break
-    case 'set-interest-rate':
-      let interest = message.content.body
-
-      // TODO - check isNumber and > 0
-      if (true) {
-        session.set('interest', interest)
-        session.set('stage', 'set-days')
-        session.reply("Please specify the number of remaining days:")
-      } else {
-        session.reply(interest + "% is not a valid value.")
-        session.set('stage', 'initialize')
-      }
-      break
-    case 'set-days':
-      let days = message.content.body
-
-      // TODO - check isNumber and > 0
-      if (true) {
-        session.set('days', days)
-        session.set('stage', 'set-consequence')
-        session.reply("Please specify the consequence:")
-      } else {
-        session.reply(days + " days is not a valid value.")
-        session.set('stage', 'initialize')
-      }
-      break
-    case 'set-consequence':
-      let consequence = message.content.body
-
-      // TODO - check null
-      if (true) {
-        session.set('consequence', consequence)
-        session.set('stage', 'set-loan')
-        sendLoan(session, 
-          "Here are your loan details:\n ETH: " + 
-          session.get('eth') + 
-          "\n Interest Rate: " + 
-          session.get('interest') + 
-          "%\n Due In: " + 
-          session.get('days') + 
-          " days\n\n Send this loan to user " + 
-          session.get('user') + 
-          "?") 
-      } else {
-        session.reply("You must specify a consequence.")
-        session.set('stage', 'initialize')
-      }
-      break
-    case 'set-loan':
-      break
-  }
+	// 'hi' is default hook to start the loan logic
+	if (message.content.body == "hi" && session.get('stage') != 'set-user'){
+		session.set('stage', 'set-contract')
+		askYesNo(session, "Would you like to make a loan contract?", 'Yes', 'No')
+		return
+	}
+	switch (session.get('stage')) {
+		case 'set-user':
+			let user = message.content.body
+			if (true) {
+				session.set('user', user)
+				setEth(session)
+				return
+			} else {
+				session.reply("User not found. Type 'hi' to try again.")
+				return
+			}
+			break
+		case 'set-amount':
+			let amount = message.content.body
+			if (true) {
+				session.set('amount', amount)
+				setInterestRate(session)
+				return
+			} else {
+				session.reply("Amount not valid. Type 'hi' to try again.")
+				return
+			}
+			break
+		case 'set-rate':
+			let rate = message.content.body
+			if (true) {
+				session.set('rate', rate)
+				setDuration(session)
+				return
+			} else {
+				session.reply("Wrong data type. Type 'hi' to try again.")
+				return
+			}
+		case 'set-duration':
+			let duration = message.content.body
+			if (true) {
+				session.set('duration', duration)
+				setConsequence(session)
+				return
+			} else {
+				session.reply("Duration is not valid. Type 'hi' to try again.")
+				return
+			}
+			break
+		case 'set-consequence':
+			let consequence = message.content.body
+			if (true) {
+				session.set('consequence', consequence)
+				finalizeLoan(session)
+				return
+			} else {
+				session.reply("Error while setting consequence. Type 'hi' to try again.")
+				return
+			}
+			break
+		session.reply("Type 'hi' to get started!")
+	}
 }
 
 function onCommand(session, command) {
-  switch (command.content.value) {
-    case 'Yes':
-      setUser(session)
-      break
-    case 'No':
-      session.set('stage', 'initialize')
-      break
-    case 'CreateLoan':
-      console.log('TRUFFLE')
-      session.set('stage', 'initialize')
-      break
-    case 'CancelLoan':
-      session.set('stage', 'initialize')
-      break
-    }
+	switch (session.get('stage')) {
+		case 'set-contract':
+			if (command.content.value == 'Yes') {
+				setUser(session)
+				return
+			} else {
+				session.reply("Operation aborted. Type 'hi' to try again.")
+				return
+			}
+			break
+		case 'finalize':
+			if (command.content.value == 'Yes') {
+				session.reply("Loan created. laksdjfsadf")
+				/*
+					LOGIC FOR PUBLISHING LOAN GOES HERE
+				*/
+			} else {
+				session.reply("Loan cancelled. Type 'hi' to try again.")
+				return
+			}
+			break
+	}
 }
 
-function onPayment(session, message) {
-  if (message.fromAddress == session.config.paymentAddress) {
-    // handle payments sent by the bot
-    if (message.status == 'confirmed') {
-      // perform special action once the payment has been confirmed
-      // on the network
-    } else if (message.status == 'error') {
-      // oops, something went wrong with a payment we tried to send!
-    }
-  } else {
-    // handle payments sent to the bot
-    if (message.status == 'unconfirmed') {
-      // payment has been sent to the ethereum network, but is not yet confirmed
-      sendMessage(session, `Thanks for the payment! 🙏`);
-    } else if (message.status == 'confirmed') {
-      // handle when the payment is actually confirmed!
-    } else if (message.status == 'error') {
-      sendMessage(session, `There was an error with your payment!🚫`);
-    }
-  }
-}
 
-// STATES
-
+// STAGE CHANGERS
 function setUser(session) {
-  session.set('stage', 'set-user')
-  session.reply('Please specify the user requesting the loan:')
+	session.set('stage', 'set-user')
+	session.reply(SOFA.Message({
+		body: "Please specify the user requesting the loan:",
+		showKeyboard: true
+	}))
 }
 
-// HELPERS
-
-function sendMessage(session, message) {
-  session.reply(SOFA.Message({
-    body: message,
-    controls: 
-      [
-        {type: 'button', label: 'Yes', value: 'Yes'},
-        {type: 'button', label: 'No', value: 'No'}
-      ],
-    showKeyboard: false,
-  }))
+function setEth(session) {
+	session.set('stage', 'set-amount')
+	session.reply(SOFA.Message({
+		body: "How much ETH would you like to lend?",
+		showKeyboard: true
+	}))
 }
 
-function sendLoan(session, message) {
-  console.log('asd')
-  session.reply(SOFA.Message({
-    body: message,
-    controls:
-      [
-        {type: 'button', label: 'Yes', value: 'CreateLoan'},
-        {type: 'button', label: 'No', value: 'CancelLoan'}
-      ],
-      showKeyboard: false,
-  }))
+function setInterestRate(session) {
+	session.set('stage', 'set-rate')
+	session.reply(SOFA.Message({
+		body: "What percent interest rate would you like to set?",
+		showKeyboard: true
+	}))
+}
+
+function setDuration(session) {
+	session.set('stage', 'set-duration')
+	session.reply(SOFA.Message({
+		body: "How many days should the loan duration be?:",
+		showKeyboard: true
+	}))
+}
+
+function setConsequence(session) {
+	session.set('stage', 'set-consequence')
+	session.reply(SOFA.Message({
+		body: "Define a consequence for defaulting on this loan.",
+		showKeyboard: true
+	}))
+}
+
+// FINALIZER
+function finalizeLoan(session) {
+	if (true) {
+		session.set('stage', 'finalize')
+		askYesNo(session, 
+			"Here are your loan details:\n>ETH: " + session.get('amount') + 
+          	"\n>Interest Rate: " + session.get('rate') + 
+          	"%\n>Due In: " + session.get('duration') + 
+          	" days\n\n Send this loan to user " + session.get('user') + "?", 
+          	"Confirm loan", "Cancel loan")
+		return
+	} else {
+		session.reply("There was an error compiling the loan. Type 'hi' to try again.")
+		return
+	}
+}
+
+// ERROR CATCHERS
+function rejectRequest(session, message) {
+	session.reply("Error: I cannot give away money")
+}
+
+function rejectPayment(session, message) {
+	session.reply("Error: I cannot accept payment until loan terms are set.")
+}
+
+// HELPERS 
+function askYesNo(session, message, yes, no) {
+	session.reply(SOFA.Message({
+		body: message,
+		controls:
+			[
+				{type: 'button', label: yes, value: 'Yes'},
+				{type: 'button', label: no, value: 'No'}
+			],
+		showKeyboard: false,
+	}))
 }
